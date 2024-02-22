8ac5cb8f130417d3c1501e6e9cacc14d3373178f
Faizan Ahmed <153298469+Faizan-fzn@users.noreply.github.com>
2024-02-22 11:27:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+   document.querySelector('#fname').addEventListener('change', capital)
+
+function capital(event){
+       // var nameInput = fname.value;
+    // var uppercaseName = nameInput.toUpperCase();
+    fname.value = fname.value.toUpperCase();; 
+}
